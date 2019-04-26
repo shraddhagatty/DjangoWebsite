50465444702618b37fa651c6511798d300a47ac6
shraddha <mailshraddh8@gmail.com>
2019-04-26 18:56:45 +0530

    models in django, populated with fake records using Faker module

--- djangoenv/Scripts/firstproject/populate_first_app.py
+++ djangoenv/Scripts/firstproject/populate_first_app.py
@@ -1,26 +1,33 @@
 import os
-os.environ.setdefault('DJANGO.SETTINGS.MODULE','first_project.settings')
+# Configure settings for project
+# Need to run this before calling models from application!
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','firstproject.settings')
 
 import django
+# Import settings
+django.setup()
 
 import random
-from first_app.models import Topic,WebPage
+from first_app.models import Topic,WebPage,AccessRecord
 from faker import Faker
 
-fakegen=Faker()
-topics=['Social','News','Market','Games']
+fakegen = Faker()
+topics = ['Search','Social','Marketplace','News','Games']
 
 def add_topic():
-    t=Topic.objects,get_or_create(top_name=random.choice(topics))[0]
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
     t.save()
     return t
+
 def populate(N=5):
     for entry in range(N):
         top=add_topic()
         furl=fakegen.url()
         fname=fakegen.company()
+        fdate=fakegen.date()
 
         webpg=WebPage.objects.get_or_create(topic=top,url=furl,name=fname)[0]
+        accrec=AccessRecord.objects.get_or_create(name=webpg,date=fdate)[0]
 
 if __name__=='__main__':
     print('Populating')
